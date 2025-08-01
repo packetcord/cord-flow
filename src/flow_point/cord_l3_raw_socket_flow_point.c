47dbad849582bf11f7d10e4cf1f8ab49d3d92127
Varban Metodiev <varban.metodiev@gmail.com>
2025-08-01 12:57:25 +0300

    CordL2RawSocketFlowPoint constructor implemented.

--- src/flow_point/cord_l3_raw_socket_flow_point.c
+++ src/flow_point/cord_l3_raw_socket_flow_point.c
@@ -1,4 +1,5 @@
 #include <flow_point/cord_l3_raw_socket_flow_point.h>
+#include <cord_error.h>
 
 static cord_retval_t CordL3RawSocketFlowPoint_rx_(CordFlowPoint const * const self, void *buffer)
 {
@@ -51,7 +52,6 @@ void CordL3RawSocketFlowPoint_ctor(CordL3RawSocketFlowPoint * const self,
     CordFlowPoint_ctor(&self->base, id, rx_buffer_size);
     self->base.vptr = &vtbl;
     self->attach_filter = &CordL3RawSocketFlowPoint_attach_filter_;
-    self->fd = fd;
     self->ifindex = ifindex;
     self->anchor_iface_name = anchor_iface_name;
     self->fanout_id = fanout_id;
@@ -59,6 +59,49 @@ void CordL3RawSocketFlowPoint_ctor(CordL3RawSocketFlowPoint * const self,
     self->ring = ring;
     self->attached_filter = attached_filter;
     self->params = params;
+
+    self->fd = fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_ALL));
+    if (self->fd < 0)
+    {
+        CORD_ERROR("CordL3RawSocketFlowPoint: socket()");
+        CORD_EXIT(EXIT_FAILURE);
+    }
+
+    if (setsockopt(self->fd, SOL_SOCKET, SO_BINDTODEVICE, self->anchor_iface_name, strlen(self->anchor_iface_name)) < 0)
+    {
+        CORD_ERROR("CordL3RawSocketFlowPoint: setsockopt(SO_BINDTODEVICE)");
+        CORD_CLOSE(self->fd);
+        CORD_EXIT(EXIT_FAILURE);
+    }
+
+    struct ifreq anchor_iface_req;
+    memset(&anchor_iface_req, 0, sizeof(struct ifreq));
+    strncpy(anchor_iface_req.ifr_name, self->anchor_iface_name, IFNAMSIZ);
+    if (ioctl(self->fd, SIOCGIFINDEX, &anchor_iface_req) < 0)
+    {
+        CORD_ERROR("CordL3RawSocketFlowPoint: ioctl(SIOCGIFINDEX)");
+    }
+    self->ifindex = anchor_iface_req.ifr_ifindex;
+
+    struct sockaddr_ll anchor_bind_addr;
+    memset(&anchor_bind_addr, 0, sizeof(struct sockaddr_ll));
+    anchor_bind_addr.sll_family = AF_PACKET;
+    anchor_bind_addr.sll_protocol = htons(ETH_P_ALL);
+    anchor_bind_addr.sll_ifindex = anchor_iface_req.ifr_ifindex;
+    if (bind(self->fd, (struct sockaddr *)&anchor_bind_addr, sizeof(struct sockaddr_ll)) < 0)
+    {
+        CORD_ERROR("CordL3RawSocketFlowPoint: bind()");
+    }
+
+    int enable = 1;
+    if (setsockopt(self->fd, SOL_PACKET, PACKET_IGNORE_OUTGOING, &enable, sizeof(enable)) < 0)
+    {
+        CORD_ERROR("CordL3RawSocketFlowPoint: setsockopt(PACKET_IGNORE_OUTGOING)");
+        CORD_CLOSE(self->fd);
+        CORD_EXIT(EXIT_FAILURE);
+    }
+
+    fcntl(self->fd, F_SETFL, O_NONBLOCK);
 }
 
 void CordL3RawSocketFlowPoint_dtor(CordL3RawSocketFlowPoint * const self)
