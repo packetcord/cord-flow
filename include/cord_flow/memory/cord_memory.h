15a65bb1fb24945675b2313f3334d9969fe81bb3
Varban Metodiev <varban.metodiev@gmail.com>
2025-08-18 12:38:18 +0300

    Adding memory aligned buffer.

--- include/cord_flow/memory/cord_memory.h
+++ include/cord_flow/memory/cord_memory.h
@@ -1,6 +1,6 @@
 #ifndef CORD_MEMORY_H
 #define CORD_MEMORY_H
 
-#define CORD_BUFFER(name, size) typedef uint8_t name[size] __attribute__ ((aligned (64)))
+#define CORD_BUFFER(name, size) uint8_t name[size] __attribute__ ((aligned (64)))
 
 #endif // CORD_MEMORY_H
