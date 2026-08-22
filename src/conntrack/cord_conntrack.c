2e07bf4f6cc9769ee904924b78d8ac233d9a2695
Varban Metodiev <213302915+packetcord@users.noreply.github.com>
2026-08-22 17:06:54 +0300

    Fixing the Clang formatting.

--- src/conntrack/cord_conntrack.c
+++ src/conntrack/cord_conntrack.c
@@ -1,11 +1,4 @@
 #include <conntrack/cord_conntrack.h>
-#include <match/cord_match.h>
-#include <cord_error.h>
-#include <cord_retval.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <sys/time.h>
 
 //
 // TBD
